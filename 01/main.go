eeb950111e2ed9fef9a5be6f6f6a4330c754bd08
Mateusz Picheta <m@picheta.dev>
2023-12-01 14:47:33 +0100

    Add day1 part2

--- 01/main.go
+++ 01/main.go
@@ -5,7 +5,32 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
+
+func buildNumLine(line string) string {
+	numLine := ""
+	for i := 0; i < len(line); i++ {
+		if _, err := strconv.Atoi(string(line[i])); err == nil {
+			numLine += string(line[i])
+		}
+	}
+	return numLine
+}
+
+func convertLettersToNumber(line string) string {
+	convertedLine := line
+	convertedLine = "one" + strings.ReplaceAll(convertedLine, "one", "1") + "one"
+	convertedLine = "two" + strings.ReplaceAll(convertedLine, "two", "2") + "two"
+	convertedLine = "three" + strings.ReplaceAll(convertedLine, "three", "3") + "three"
+	convertedLine = "four" + strings.ReplaceAll(convertedLine, "four", "4") + "four"
+	convertedLine = "five" + strings.ReplaceAll(convertedLine, "five", "5") + "five"
+	convertedLine = "six" + strings.ReplaceAll(convertedLine, "six", "6") + "six"
+	convertedLine = "seven" + strings.ReplaceAll(convertedLine, "seven", "7") + "seven"
+	convertedLine = "eight" + strings.ReplaceAll(convertedLine, "eight", "8") + "eight"
+	convertedLine = "nine" + strings.ReplaceAll(convertedLine, "nine", "9") + "nine"
+	return convertedLine
+}
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -18,18 +43,13 @@ func main() {
 	sum := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		line := string(scanner.Text())
+		line := scanner.Text()
+		line = convertLettersToNumber(line)
 
-		numLine := ""
-
-		for i := 0; i < len(line); i++ {
-			if _, err := strconv.Atoi(string(line[i])); err == nil {
-				numLine += string(line[i])
-			}
-		}
-
-		lineSum := string(numLine[0]) + string(numLine[len(numLine)-1])
-		num, err := strconv.ParseInt(lineSum, 10, 64)
+		numLine := buildNumLine(line)
+		trunk := string(numLine[0]) + string(numLine[len(numLine)-1])
+		fmt.Println(trunk)
+		num, err := strconv.ParseInt(trunk, 10, 64)
 		if err == nil {
 			sum += int(num)
 		}
