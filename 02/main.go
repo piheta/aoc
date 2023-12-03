4a018793a286ba5f20706bc6ecd68c2d25236df3
tama3ti <m.picheta@icloud.com>
2023-12-03 01:49:58 +0100

    Add day1part1 solution

--- 02/main.go
+++ 02/main.go
@@ -4,9 +4,41 @@ import (
     "bufio"
     "fmt"
     "os"
+    "strconv"
+    "strings"
 )
 
+var RED_LIMIT = 12
+var GREEN_LIMIT = 13
+var BLUE_LIMIT = 14
+
+func isLinePossible(line string) bool {
+    r,g,b := 0, 0, 0
+
+    for i := 0; i < len(line); i++ {
+        if num, err := strconv.Atoi(string(line[i])); err == nil { //if char is a number
+            if numleft, err := strconv.Atoi(string(line[i-1])); err == nil { //check for 2digit number
+                num += numleft*10
+            }
+
+            if (string(line[i+2]) == "r") {
+                r += num
+            } else if (string(line[i+2]) == "g") {
+                g += num
+            } else if (string(line[i+2]) == "b") {
+                b += num
+            }
+
+        } else if (string(line[i]) == ";" && (r <= RED_LIMIT && g <= GREEN_LIMIT && b <= BLUE_LIMIT)) {
+            r,g,b = 0, 0, 0
+        }
+    }
+
+    return (r <= RED_LIMIT && g <= GREEN_LIMIT && b <= BLUE_LIMIT)
+}
+
 func main() {
+    sum := 0
     file, err := os.Open("input.txt")
     if err != nil {
         fmt.Println("Error opening file", err)
@@ -17,8 +49,20 @@ func main() {
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
         line := scanner.Text()
-        fmt.Println(line)
+
+		if isLinePossible(line) {
+			line = strings.TrimPrefix(line, "Game ")
+			parts := strings.Split(line, ":")
+			value, err := strconv.Atoi(parts[0])
+			if err != nil {
+				fmt.Println("Error converting to integer:", err)
+				continue
+			}
+			sum += value		
+        }
     }
+
+    fmt.Println(sum)
 
     if err := scanner.Err(); err != nil {
         fmt.Println("Error reading file", err)
