4b5436b849c248c487d2887290f6a32f8b7767e1
Mateusz Picheta <m@picheta.dev>
2023-12-04 15:23:08 +0100

    Add day4par1 solution

--- 04/main.go
+++ 04/main.go
@@ -1,26 +1,65 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strings"
 )
 
+func implContains[T comparable](s []T, e T) bool {
+	for _, v := range s {
+		if v == e {
+			return true
+		}
+	}
+	return false
+}
+
+func findCardTotal(line string) float64 {
+	cardTotal := 0
+
+	line = strings.ReplaceAll(line, " |", "")
+	line = strings.ReplaceAll(line, "  ", " ")
+
+	_, nums, _ := strings.Cut(line, ": ")
+
+	numArr := strings.Split(nums, " ")
+	winners := []string{}
+
+	for i := 0; i < len(numArr); i++ {
+		if i < 10 {
+			winners = append(winners, numArr[i])
+		} else if implContains(winners, numArr[i]) {
+			cardTotal++
+		}
+	}
+
+	if cardTotal == 0 {
+		return 0
+	}
+
+	return math.Pow(2.0, (float64(cardTotal) - 1))
+}
+
 func main() {
-    file, err := os.Open("input.txt")
-    if err != nil {
-        fmt.Println("Error opening file", err)
-        return
-    }
-    defer file.Close()
+	sum := 0.0
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening file", err)
+		return
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        line := scanner.Text()
-        fmt.Println(line)
-    }
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		sum += findCardTotal(line)
+	}
 
-    if err := scanner.Err(); err != nil {
-        fmt.Println("Error reading file", err)
-    }
+	fmt.Println(sum)
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file", err)
+	}
 }
